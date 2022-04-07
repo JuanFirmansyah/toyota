99e7a868d10214e4484f937eff8987f6eccd9e26
JuanFirmansyah <juanfirmansyah.jf@gmail.com>
2022-04-07 17:23:54 +0700

    revisi

--- components/Background.js
+++ components/Background.js
@@ -5,13 +5,13 @@ function Background() {
   return (
     <section className="flex flex-col w-full h-[116vh] z-10">
       <ReactPlayer
-        url="https://www.youtube.com/watch?v=q_ZpE15892U"
+        url="https://www.youtube.com/watch?v=QVRQeV0Af3Q"
         playing={true}
         controls={false}
         width="100%"
         height="100%"
         loop={true}
-        muted={false}
+        muted={true}
         playsinline={true}
       />
       <div className="absolute text-white w-full text-center top-24"></div>
