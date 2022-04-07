62635e015c4ea0c5c4fdd37eb919a3493093265c
JuanFirmansyah <juanfirmansyah.jf@gmail.com>
2022-04-07 17:12:23 +0700

    finished

--- components/Background.js
+++ components/Background.js
@@ -3,15 +3,15 @@ import ReactPlayer from 'react-player';
 
 function Background() {
   return (
-    <section className="flex flex-col w-full h-auto z-10">
+    <section className="flex flex-col w-full h-[116vh] z-10">
       <ReactPlayer
-        url="honda.MP4"
+        url="https://www.youtube.com/watch?v=q_ZpE15892U"
         playing={true}
         controls={false}
         width="100%"
         height="100%"
         loop={true}
-        muted={true}
+        muted={false}
         playsinline={true}
       />
       <div className="absolute text-white w-full text-center top-24"></div>
